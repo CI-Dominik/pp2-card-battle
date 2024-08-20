27dac063d6a4c719886c80ea2fb94eea54c280c9
Dominik Wulf <dominik.wulf@students.codeinstitute.net>
2024-08-20 09:58:24 +0200

    Add basic function structure

---
+++ assets/js/scrips.js
@@ -0,0 +1,52 @@
+// Declare global variables
+
+let cards = [];
+let handCards = [];
+
+/** Function to load when DOM content is loaded */
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    
+
+});
+
+/** Initiate cards to card deck */
+
+function initCards() {
+
+
+
+}
+
+/** Declare number of cards to draw */
+
+function drawCards() {
+
+
+
+}
+
+/** Load a random enemy to DOM */
+
+function loadEnemy() {
+
+
+
+}
+
+/** Attack Phase Script */
+
+function attackPhase() {
+
+
+
+}
+
+/** Defense Phase Script */
+
+function defensePhase() {
+
+
+
+}
