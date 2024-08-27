cc0565849a36e4a9caccdef31f9ec693d0d75360
Dominik Wulf <dominik.wulf@students.codeinstitute.net>
2024-08-27 09:15:58 +0200

    Add specials to Card object

--- assets/js/Card.js
+++ assets/js/Card.js
@@ -2,12 +2,14 @@
 
 class Card {
 
-    constructor(name, attack, defense, special, description, maxAmount) {
+    constructor(name, attack, defense, specialDescription, description, maxAmount) {
 
         this.name = name;
         this.attack = attack;
         this.defense = defense;
-        this.special = special;
+        this.specialDescription = specialDescription;
+        this.specialType = specialType;
+        this.specialValue = specialValue;
         this.description = description;
         this.maxAmount = maxAmount;
 
@@ -19,12 +21,12 @@ class Card {
 
 const cardPresets = [
 
-        new Card("Test1", 20, 30, "PH", "PH", 3),
-        new Card("Test2", 50, 80, "PH", "PH", 1),
-        new Card("Test3", 10, 20, "PH", "PH", 3),
-        new Card("Test4", 100, 75, "PH", "PH", 3),
-        new Card("Test5", 45, 23, "PH", "PH", 2),
-        new Card("Test6", 5, 10, "PH", "PH", 10)
+        new Card("Test1", 20, 30, "PH", "PH", "PH", 3),
+        new Card("Test2", 50, 80, "PH", "PH", "PH", 1),
+        new Card("Test3", 10, 20, "PH", "PH", "PH", 3),
+        new Card("Test4", 100, 75, "PH", "PH", "PH", 3),
+        new Card("Test5", 45, 23, "PH", "PH", "PH", 2),
+        new Card("Test6", 5, 10, "PH", "PH", "PH", 10)
 
 ]
 
