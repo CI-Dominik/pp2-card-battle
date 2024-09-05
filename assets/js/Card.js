a8e1b4a7793b9beb0845bb3204748aaea9c53960
Dominik Wulf <dominik.wulf@students.codeinstitute.net>
2024-09-05 16:56:34 +0200

    Re-enable all cards and load more enemies

--- assets/js/Card.js
+++ assets/js/Card.js
@@ -24,13 +24,13 @@ class Card {
 const cardPresets = [
 
         new Card("Doctor Goodman", 20, 30, "Special Description", "attack", "Special Type", "Special Value", "Description", "doctor", 3),
-        /* new Card("Talonus", 50, 80, "Special Description", "defense", "Special Type", "Special Value", "Description", "eagle-warrior", 1),
+        new Card("Talonus", 50, 80, "Special Description", "defense", "Special Type", "Special Value", "Description", "eagle-warrior", 1),
         new Card("Shamus", 10, 20, "Special Description", "attack", "Special Type", "Special Value", "Description", "forest-shaman", 3),
         new Card("Ingrid", 100, 75, "Special Description", "none", "Special Type", "Special Value", "Description", "forest-spirit", 3),
         new Card("Magus", 45, 23, "Special Description", "none", "Special Type", "Special Value", "Description", "mage", 2),
         new Card("Rambo", 10, 10, "Special Description", "attack", "Special Type", "Special Value", "Description", "mercenary", 1),
         new Card("Dominik, the Bearded One", 10, 10, "Special Description", "defense", "Special Type", "Special Value", "Description", "dominik", 1),
-        new Card("Daniela, the Owl Witch", 10, 10, "Special Description", "defense", "Special Type", "Special Value", "Description", "daniela", 1), */
+        new Card("Daniela, the Owl Witch", 10, 10, "Special Description", "defense", "Special Type", "Special Value", "Description", "daniela", 1),
         new Card("Frederic, the Mad Genius", 50, 10, "Special Description", "defense", "Special Type", "Special Value", "Description", "frederic", 1),
         new Card("Alicja, the Radiant", 10, 10, "Special Description", "defense", "Special Type", "Special Value", "Description", "alicja", 1),
         new Card("Lentje, the Crazy Chicken Lady", 50, 10, "Special Description", "defense", "Special Type", "Special Value", "Description", "lentje", 1),
