71b374911858caca4d24ab271ad5bdf03df368cf
Dominik Wulf <dominik.wulf@students.codeinstitute.net>
2024-09-11 13:25:25 +0200

    Increase amount of attack cards and decrease amount of defense cards

--- assets/js/Card.js
+++ assets/js/Card.js
@@ -24,12 +24,12 @@ class Card {
 
 const cardPresets = [
 
-        new Card("Doctor Goodman", 5, 15, "DEFENSE PHASE: HoT effect for 10 HP over 2 rounds", "defense", "hot", 10, 2, "A highly intelligent and famous doctor who revolutionized the modern steampunk world.", "doctor", 7),
-        new Card("Nest Protector", 40, 5, "No special skill", "none", "none", 0, 0, "A proud avian warrior who was sent out to protect his land from evil.", "eagle-warrior", 5),
-        new Card("Woodland Shaman", 5, 30, "ATTACK PHASE: DoT effect for 8 damage over 5 rounds", "attack", "dot", 15, 5, "Coming from the lands of the unknown, the Woodland Shaman inflicts curses to his enemies.", "forest-shaman", 5),
-        new Card("Irydil", 5, 40, "No special skill", "none", "none", 0, 0, "A forest spirit sent by the nature gods to cleanse the woods of threats.", "forest-spirit", 6),
-        new Card("Magna", 35, 10, "No special skill", "none", "none", 0, 0, "An arcane sorceress who gains her power from ancient spirits of the earth.", "mage", 4),
-        new Card("Mercenary", 40, 10, "No special skill", "none", "none", 0, 0, "A mercenary, here for one reason: Combat.", "mercenary", 5),
+        new Card("Doctor Goodman", 5, 15, "DEFENSE PHASE: HoT effect for 10 HP over 2 rounds", "defense", "hot", 10, 2, "A highly intelligent and famous doctor who revolutionized the modern steampunk world.", "doctor", 5),
+        new Card("Nest Protector", 40, 5, "No special skill", "none", "none", 0, 0, "A proud avian warrior who was sent out to protect his land from evil.", "eagle-warrior", 7),
+        new Card("Woodland Shaman", 5, 30, "ATTACK PHASE: DoT effect for 8 damage over 5 rounds", "attack", "dot", 15, 5, "Coming from the lands of the unknown, the Woodland Shaman inflicts curses to his enemies.", "forest-shaman", 4),
+        new Card("Irydil", 5, 40, "No special skill", "none", "none", 0, 0, "A forest spirit sent by the nature gods to cleanse the woods of threats.", "forest-spirit", 5),
+        new Card("Magna", 35, 10, "No special skill", "none", "none", 0, 0, "An arcane sorceress who gains her power from ancient spirits of the earth.", "mage", 6),
+        new Card("Mercenary", 40, 10, "No special skill", "none", "none", 0, 0, "A mercenary, here for one reason: Combat.", "mercenary", 6),
         new Card("Dominik, the Bearded One", 30, 15, "ATTACK PHASE: Stun enemy for 4 rounds", "attack", "stun", 0, 4, "Famous for his beard and power, he conquers even the greatest foes.", "dominik", 1),
         new Card("Daniela, the Owl Witch", 15, 35, "DEFENSE PHASE: HoT effect of 12 HP over 4 rounds", "defense", "hot", 12, 4, "Dominik's loyal and harmonious girlfriend. Able to heal even the biggest wounds.", "daniela", 1),
         new Card("Frederic, the Mad Genius", 25, 25, "DEFENSE PHASE: Stun enemy for 3 rounds", "defense", "stun", 0, 3, "Dominik's clever advisor who finds solutions for every problem he encounters.", "frederic", 1),
